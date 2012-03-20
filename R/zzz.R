7b3b0f19f6e67444fd5e851050f4550ec833a443
Cathy W. S. Chen <chenws@fcu.edu.tw>
2012-03-20 00:00:00 +0000

    version 0.2-5

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onAttach <- function(...)
+.onAttach <- function(lib,pkg)
 {
 
 
@@ -8,8 +8,13 @@
    this.year <- substr(date, x[1], x[1] + attr(x, "match.length") - 1)
 
    # echo output to screen
-   cat("##\n## On Bayesian analysis of Threshold autoregressive model (BAYSTAR)\n")
-   cat("## Copyright (C) 2007-", this.year,
+   packageStartupMessage("##\n## On Bayesian analysis of Threshold autoregressive model (BAYSTAR)\n")
+   packageStartupMessage("## Copyright (C) 2007-", this.year,
       " Cathy W. S. Chen, Edward M.H. Lin, F.C. Liu, and Richard Gerlach\n", sep="")
-   require(mvtnorm, quietly=TRUE)
+ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
+     ver <- as.character(ver)	
+    packageStartupMessage("Version: ", ver, "loaded\n")
+
+#   require(mvtnorm, quietly=TRUE)
+#   require(coda, quietly=TRUE)
 }
